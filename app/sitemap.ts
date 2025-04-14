e8cfd83c8021ccb1485aad0d2ced696a3964d98c
Rushy <michael@rushyservers.com>
2025-04-14 16:25:41 +1000

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://luvalotfashion.com',
+      url: 'https://luvalot.rushyservers.com',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1.0,
